d64390106f5d054d841458ef0b7487641239cd88
santiago <santiagocendra.sc@gmail.com>
2021-05-06 20:29:17 -0300

    animaciones jquery

---
+++ js/animaciones.js
@@ -0,0 +1,16 @@
+
+
+//Efectos con jQuery
+//Al apretar aparece y da las gracias
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideDown(2000);
+      $("#panel").slideUp(2000);
+    });
+  });
+
+
+  //Efecto bienvenida index
+  $("#efecto-index").prepend('<h3 >Bienvenidos a Sagrada Pasteleria!!</h3>');
+  $("h3").fadeOut(4000); 
--- js/formularios.js
+++ js/formularios.js
@@ -12,11 +12,6 @@ $('#formulario').submit(function(e){
     console.log(name + "\n" + email + "\n" +subject + "\n" + direccion + "\n" + message)
 })
 
-//Efectos con jQuery
-//Al apretar aparece y da las gracias
 
-$(document).ready(function(){
-    $("#flip").click(function(){
-      $("#panel").slideDown("slow");
-    });
-  });
+
+  
