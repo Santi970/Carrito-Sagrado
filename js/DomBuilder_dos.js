35b82040a8ffc3368dfcecc26ca7a64f7d9c941c
santiago <santiagocendra.sc@gmail.com>
2021-05-12 00:51:18 -0300

    cambios en el jquery

--- js/DomBuilder_dos.js
+++ js/DomBuilder_dos.js
@@ -1,4 +1,4 @@
-class DomBuilder{
+class DomBuilder_productos{
 
     getCardHtml = (product) => {
         return `
@@ -16,6 +16,68 @@ class DomBuilder{
     `;
     }
 
+
+    getCardHtml_carrito = (product) => {
+        return `
+        <div id="template-carrito">
+        <tr>
+          <th scope="row">id</th>
+          <td>${product.title}</td>
+          <td>${product.cantidad}</td>
+          <td>
+              <button class="btn btn-outline-success btn-sm">
+                  +
+              </button>
+              <button class="btn  btn-outline-warning btn-sm">
+                  -
+              </button>
+          </td>
+          <td>$ <span>${product.precio}</span></td>
+        </tr>
+      </div>               
+    `;
+    }
+
+
+    getCardHtml_footCarrito = (product) => {
+        return `
+        <div id="template-footer">
+        <th scope="row" colspan="2">Total productos</th>
+        <td>10</td>
+        <td>
+            <button class="btn btn-danger btn-sm" id="vaciar-carrito">
+                vaciar todo
+            </button>
+     
+        </td>
+        <td class="font-weight-bold">$ <span>5000</span></td>
+        </div>              
+    `;
+    }
+
+    getCardHtml_carritoVacio = (product) => {
+        return `
+        <table class="table">
+        <thead>
+
+          <tr>
+            <th scope="col"><i class="fas fa-chevron-circle-down"></i></th>
+            <th scope="col">Compras</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col"><i class="fas fa-chevron-circle-down"></i></th>
+            <th scope="col">Total</th>
+          </tr>
+        </thead>
+        <tbody id="items"></tbody>
+        <tfoot>
+          <tr id="footer">
+            <th scope="row" colspan="5">Carrito vacío</th>
+          </tr>
+        </tfoot>
+      </table>              
+    `;
+    }
+
     clearDom = (parent) => {
         parent.empty();
     }
@@ -26,6 +88,138 @@ class DomBuilder{
             parent.append(this.getCardHtml(product));
         });
     }
+
+    renderCarrito = (carrito,parent_carrito) => {
+        this.clearDom(parent_carrito);
+        carrito.forEach(product => {
+            parent_carrito.append(this.getCardHtml_carrito(product));
+        });
+    }
+
+    renderFootCarrito = (products,parent_footer) => {
+        this.clearDom(parent_footer);
+        products.forEach(product => {
+            parent_footer.append(this.getCardHtml_footCarrito(product));
+        });
+    }
+
+    renderCarritoVacio = (products,parent_table) => {
+        this.clearDom(parent_table);
+        products.forEach(product => {
+            parent_table.append(this.getCardHtml_carritoVacio(product));
+        });
+    }
+
+
 }
 
-export default DomBuilder;
+export default DomBuilder_productos;
+
+
+//template-carrito
+class DomBuilder_carrito{
+
+    getCardHtml_carrito = (product) => {
+        return `
+        <div id="template-carrito">
+        <tr>
+          <th scope="row">id</th>
+          <td>${product.title}</td>
+          <td>${product.cantidad}</td>
+          <td>
+              <button class="btn btn-outline-success btn-sm">
+                  +
+              </button>
+              <button class="btn  btn-outline-warning btn-sm">
+                  -
+              </button>
+          </td>
+          <td>$ <span>${product.precio}</span></td>
+        </tr>
+      </div>               
+    `;
+    }
+
+    clearDom = (parent_carrito) => {
+        parent_carrito.empty();
+    }
+
+    renderProducts = (products,parent_carrito) => {
+        this.clearDom(parent_carrito);
+        products.forEach(product => {
+            parent_carrito.append(this.getCardHtml_carrito(product));
+        });
+    }
+}
+
+
+
+//template-footer
+class DomBuilder_fotCarrito{
+
+    getCardHtml_footCarrito = (product) => {
+        return `
+        <div id="template-footer">
+        <th scope="row" colspan="2">Total productos</th>
+        <td>10</td>
+        <td>
+            <button class="btn btn-danger btn-sm" id="vaciar-carrito">
+                vaciar todo
+            </button>
+     
+        </td>
+        <td class="font-weight-bold">$ <span>5000</span></td>
+        </div>              
+    `;
+    }
+
+    clearDom = (parent_footer) => {
+        parent_footer.empty();
+    }
+
+    renderProducts = (products,parent_footer) => {
+        this.clearDom(parent_footer);
+        products.forEach(product => {
+            parent_footer.append(this.getCardHtml_footCarrito(product));
+        });
+    }
+}
+
+
+//template footer carrito
+class DomBuilder_carritoVacio{
+
+    getCardHtml_carritoVacio = (product) => {
+        return `
+        <table class="table">
+        <thead>
+
+          <tr>
+            <th scope="col"><i class="fas fa-chevron-circle-down"></i></th>
+            <th scope="col">Compras</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col"><i class="fas fa-chevron-circle-down"></i></th>
+            <th scope="col">Total</th>
+          </tr>
+        </thead>
+        <tbody id="items"></tbody>
+        <tfoot>
+          <tr id="footer">
+            <th scope="row" colspan="5">Carrito vacío</th>
+          </tr>
+        </tfoot>
+      </table>              
+    `;
+    }
+
+    clearDom = (parent_table) => {
+        parent_table.empty();
+    }
+
+    renderProducts = (products,parent_table) => {
+        this.clearDom(parent_table);
+        products.forEach(product => {
+            parent_table.append(this.getCardHtml_carritoVacio(product));
+        });
+    }
+}
--- js/app_jquery.js
+++ js/app_jquery.js
@@ -1,8 +1,13 @@
-import DomBuilder from "../js/DomBuilder_dos.js";
+import DomBuilder_productos from "../js/DomBuilder_dos.js";
 
 //NUEVO
-const domBuilder = new DomBuilder();
+const domBuilder = new DomBuilder_productos();
 const parent =$('#cards')
+const parent_carrito = $('#template-carrito')
+const parent_footer = $('#template-footer')
+const parent_table = $('#table')
+
+
 let carrito = {}
 
 
@@ -29,31 +34,54 @@ $(() => {
 //Pintamos Cards con Builder   
 const pintarCards = data =>{
     domBuilder.renderProducts(data, parent)
-
 }
+
+
+
+
 
 const addCarrito = e => {
     console.log(e.target)
     if((e.target.classList.contains('btn-secondary'))){
-        //console.log(e.target.parentElement)
         setCarrito(e.target.parentElement)
     }
     e.stopPropagation()
 }
 
-//empujamos los objetos al carrito. 
+//Empujamos los objetos al carrito. 
 const setCarrito = objeto => {
 
     
     const producto = {
 
-     
-        id: $('.btn-secondary').data(objeto.id)
+        id: objeto.querySelector('.btn-secondary').dataset.id,
+        title: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad: 1
     }
-    console.log(producto)
 
+    //si el producto existe aumenta la cantidad
+    if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
 
+    //Con los tres puntitos hacemos una copia de "producto".
+    //creamos el index con su producto.id si existe se sobreescribe.  
+    carrito[producto.id] = {...producto}
+
+    pintarCarrito()
 }
 
+const pintarCarrito = () => {
 
+    domBuilder.getCardHtml_carrito(carrito,parent_carrito)
+
+
+    Object.values(carrito).forEach(producto => {
+        //tengo que seguir pero hasta aca funciona perfecto
+
+       
+
+    })
+}
 
